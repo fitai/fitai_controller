f8f7f9ade1ae5ec351ae97714be6405087f2dab4
FluffiKitten <j.kylebrubaker@gmail.com>
2017-01-07 21:17:32 -0500

    uncomments print.

--- comms/update_redis.py
+++ comms/update_redis.py
@@ -83,7 +83,7 @@ def main(args):
         elif not response:
             print 'Redis object not updated properly. Will not increment lift_id.'
         elif not update_lift_id:
-            # print 'JSON object did not include lift_id. Should be a trigger to end lift and stop pushing to db'
+            print 'JSON object did not include lift_id. Should be a trigger to end lift and stop pushing to db'
             # print 'found lift_id: {}'.format(collar['lift_id'])
             print lift_to_json(collar['lift_id'])
         else:
