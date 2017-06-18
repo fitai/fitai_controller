1d878546cca9f6c06fbf4b8fae712567bde1e3b6
jbrubaker <j.kylebrubaker@gmail.com>
2017-06-17 21:20:55 -0400

    minor cleaning on mqtt_client and redis_pubsub

--- comms/redis_pubsub.py
+++ comms/redis_pubsub.py
@@ -1,14 +1,6 @@
 from datetime import datetime as dt
 from json import dumps
 
-# msg_dict = {"header": 'test',
-#             "rep_count": 'test',
-#             "content":
-#                 {"v_rms": [0, 1, 2, 3, 4],
-#                  "p_rms": [0, 2, 3, 4, 5]}
-#             }
-# msg = dumps(msg_dict)
-
 
 def prep_message(collar_obj, vel, pwr):
     if isinstance(collar_obj['lift_start'], type(dt.now())):
@@ -27,8 +19,6 @@ def redis_pub(redis_client, collar_obj, vals):
     vel, pwr = vals[1], vals[2]  # pull out relevant entries
 
     msg = prep_message(collar_obj, vel, pwr)
-
-    # print 'message to redis pubsub: \n{}'.format(msg)
 
     if redis_client is not None:
         redis_client.publish('lifts', msg)
